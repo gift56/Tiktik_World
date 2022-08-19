38c0d78ad3a6d20bbf0c606600d5f80e21fe2d61
gift56 <efeasiughu@gmail.com>
2022-08-19 17:08:25 +0100

    done working for today good night

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,8 +1,9 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-
-
+import { allPostsQuery } from "../../../utils/queries";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  res.status(200).json({ name: "Response Success" });
+  if (req.method === "GET") {
+    const query = allPostsQuery();
+  }
 }
