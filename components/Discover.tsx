c3c923d6ee34774650cde26b16bef22385f02e67
gift56 <efeasiughu@gmail.com>
2022-08-19 16:04:01 +0100

    created topic styles

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -6,7 +6,9 @@ import { topics } from "../utils/constants";
 const Discover = () => {
   const activeTopicStyle = "";
 
-  const topicStyle = "";
+  const topicStyle =
+    "xl:border-2 hover:bg-primary xl:border-gray-300 px-3 py-2 rounded xl:rounded-full flex items-center gap-2 justify-center cursor-pointer text-black";
+
   return (
     <div className="xl:border-b-2 xl:border-gray-200 pb-6">
       <p className="tex-gray-500 font-semibold m-3 mt-4 hidden xl:block">
@@ -15,7 +17,7 @@ const Discover = () => {
       <div className="flex gap-3 flex-wrap">
         {topics.map((item) => (
           <Link href={`/?topic=${item.name}`} key={item.name}>
-            <div>
+            <div className={topicStyle}>
               <span className="text-2xl font-bold xl:text-md">{item.icon}</span>
               <span className="font-medium text-md hidden xl:block capitalize">
                 {item.name}
