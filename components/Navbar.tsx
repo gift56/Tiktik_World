a071d51f0eeccd767b7940eb07b377f85ad60198
gift56 <efeasiughu@gmail.com>
2022-08-19 14:34:13 +0100

    started working on navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,9 +6,23 @@ import { GoogleLogin, GoogleLogout } from "react-google-login";
 import { AiOutlineLogout } from "react-icons/ai";
 import { BiSearch } from "react-icons/bi";
 import { IoMdAdd } from "react-icons/io";
+import Logo from "../utils/tiktik-logo.png";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div>
+      <Link href="/">
+        <div>
+          <Image
+            className="cursor-pointer"
+            src={Logo}
+            alt="tiktik"
+            layout="responsive"
+          />
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Navbar;
