a9fd0c57ef506b7ce5515c502812d24ecefd4c4f
gift56 <efeasiughu@gmail.com>
2022-08-19 16:13:51 +0100

    started working on sidebar footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React from "react";
+import { footerList1, footerList2, footerList3 } from "../utils/constants";
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
-}
+  return <div className="mt-6 hidden xl:block"></div>;
+};
 
-export default Footer
+export default Footer;
