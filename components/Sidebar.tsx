878e072592bf2ebf3d60e926a0d02b2dc50f20ae
gift56 <efeasiughu@gmail.com>
2022-08-19 15:20:51 +0100

    still workin on sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,6 +8,12 @@ import { ImCancelCircle } from "react-icons/im";
 
 const Sidebar = () => {
   const [showSidebar, setshowSidebar] = useState(true);
+
+  const userProfile = false;
+
+  const normalLink =
+    "flex items-center justify-center xl:justify-start cursor-pointer font-semibold text-[#f51997] rounded gap-3 hover:bg-primary p-3";
+
   return (
     <div>
       <div
@@ -18,7 +24,21 @@ const Sidebar = () => {
       </div>
       {showSidebar && (
         <div className="xl:w-400 xl:border-0 w-20 flex flex-col justify-start mb-10 border-r-2 border-gray-100 p-3">
-          <div></div>
+          <div className="xl:border-b-2 border-gray-200 xl:pb-4">
+            <Link href="/">
+              <div className={normalLink}>
+                <p className="text-2xl">
+                  <AiFillHome />
+                </p>
+                <span className="text-xl hidden xl:block">For You</span>
+              </div>
+            </Link>
+          </div>
+          {!userProfile && (
+            <div className="px-2 py-4 hidden xl:block">
+                <p className="text-gray-400">Log in to like and comment on videos</p>
+            </div>  
+          )}
         </div>
       )}
     </div>
