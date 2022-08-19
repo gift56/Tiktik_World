2e096543d9567bb863ce4ae59c9ed0c2a12938c9
gift56 <efeasiughu@gmail.com>
2022-08-19 16:15:01 +0100

    done with todays work

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,11 @@ import React from "react";
 import { footerList1, footerList2, footerList3 } from "../utils/constants";
 
 const Footer = () => {
-  return <div className="mt-6 hidden xl:block"></div>;
+  return <div className="mt-6 hidden xl:block">
+    <div className="flex flex-wrap">
+        
+    </div>
+  </div>;
 };
 
 export default Footer;
