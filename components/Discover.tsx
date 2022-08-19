6d964360edd73b0d8b174a9deb65f530f27380bb
gift56 <efeasiughu@gmail.com>
2022-08-19 15:53:28 +0100

    successfully shown some items

--- components/Discover.tsx
+++ components/Discover.tsx
@@ -4,7 +4,22 @@ import { useRouter } from "next/router";
 import { topics } from "../utils/constants";
 
 const Discover = () => {
-  return <div>Discover</div>;
+  return (
+    <div className="xl:border-b-2 xl:border-gray-200 pb-6">
+      <p className="tex-gray-500 font-semibold m-3 mt-4 hidden xl:block">
+        Popular Topics
+      </p>
+      <div className="flex gap-3 flex-wrap">
+        {topics.map((item) => (
+          <Link href={`/?topic=${item.name}`} key={item.name}>
+            <div>
+              <span>{item.icon}</span>
+            </div>
+          </Link>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Discover;
