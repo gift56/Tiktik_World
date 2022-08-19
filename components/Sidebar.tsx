70a8099cea1a9ae5edf1edade36b5ee1bad38434
gift56 <efeasiughu@gmail.com>
2022-08-19 14:47:30 +0100

    started working on mobile view

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React, { useState } from "react";
+import { NextPage } from "next";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { GoogleLogin } from "react-google-login";
+import { AiFillHome, AiOutlineMenu } from "react-icons/ai";
+import { ImCancelCircle } from "react-icons/im";
 
 const Sidebar = () => {
+  const [showSidebar, setshowSidebar] = useState(false);
   return (
-    <div>Sidebar</div>
-  )
-}
+    <div>
+      <div className="block xl:hidden m-2 ml-4 mt-3 text-xl"></div>
+    </div>
+  );
+};
 
-export default Sidebar
+export default Sidebar;
