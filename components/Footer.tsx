48491bd386d02ab0b48a37732be7690e27527ebf
gift56 <efeasiughu@gmail.com>
2022-08-19 16:24:09 +0100

    passed footerlist 1 as props

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,25 @@
 import React from "react";
 import { footerList1, footerList2, footerList3 } from "../utils/constants";
 
+const List = ({ items }: { items: string[] }) => (
+  <div className="flex flex-wrap gap-2 mt-5">
+    {items.map((item) => (
+      <p
+        key={item}
+        className="text-gray-400 text-sm hover:underline cursor-pointer"
+      >
+        {item}
+      </p>
+    ))}
+  </div>
+);
+
 const Footer = () => {
-  return <div className="mt-6 hidden xl:block">
-    <div className="flex flex-wrap">
-        
+  return (
+    <div className="mt-6 hidden xl:block">
+      <List items={footerList1} />
     </div>
-  </div>;
+  );
 };
 
 export default Footer;
