737a399b0c0129f2c767f254ed245c1cd8996f86
gift56 <efeasiughu@gmail.com>
2022-08-19 12:24:33 +0100

    working on my back end

---
+++ sanity-backend/schemas/user.js
@@ -0,0 +1,15 @@
+export default {
+  name: "user",
+  title: "User",
+  type: "document",
+  fields: [
+    {
+      name: "userName",
+      title: "User Name",
+      type: "string",
+    },
+    {
+      name: "image",
+    },
+  ],
+};
