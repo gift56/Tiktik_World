a122950c923e99d87b248947e66837642bd5845a
gift56 <efeasiughu@gmail.com>
2022-08-19 14:39:31 +0100

    started stying img in navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -10,9 +10,9 @@ import Logo from "../utils/tiktik-logo.png";
 
 const Navbar = () => {
   return (
-    <div className="w-full flex justify-between items-center border-b-2">
+    <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
-        <div>
+        <div className="w-[100px] md:w-[130px]">
           <Image
             className="cursor-pointer"
             src={Logo}
