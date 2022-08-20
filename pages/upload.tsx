e56aa9b4f8d36084e029d5270841d1e56aa86d92
gift56 <efeasiughu@gmail.com>
2022-08-20 16:27:02 +0100

    added transition

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -15,7 +15,7 @@ const Upload = () => {
             <p className="text-2xl font-bold">Upload a Video</p>
             <p className="text-md text-gray-400">Post videos to your account</p>
           </div>
-          <div className="border-dashed rounded-xl border-4 border-gray-200 flex flex-col justify-center items-center outline-none mt-10 w-[260px]"></div>
+          <div className="border-dashed rounded-xl border-4 border-gray-200 flex flex-col justify-center items-center outline-none mt-10 w-[260px] h-[460px] p-10 cursor-pointer hover:border-red-300 hover:bg-gray-100 transition-all transition-3s"></div>
         </div>
       </div>
     </div>
