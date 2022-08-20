3ce49125cca05eda56d7ceb7004c4e6da2e6445e
gift56 <efeasiughu@gmail.com>
2022-08-20 09:57:15 +0100

    starting to code

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,9 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import { client } from "../../../utils/client";
 import { allPostsQuery } from "../../../utils/queries";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "GET") {
     const query = allPostsQuery();
+
+    // const data = await client;
   }
 }
