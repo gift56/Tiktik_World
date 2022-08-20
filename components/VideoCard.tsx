bbf292e59ac63021c4109b312bfc754a4cd22afd
gift56 <efeasiughu@gmail.com>
2022-08-20 11:45:21 +0100

    still working on profile page

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -15,7 +15,30 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
   return (
     <div className="flex flex-col pb-6 border-b-2 border-gray-200">
       <div>
-        
+        <div className="flex gap-3 p-2 cursor-pointer font-semibold rounded">
+          <div className="md:w-16 md:h-16 w-10 h-10">
+            <Link href="/">
+              <>
+                <Image
+                  width={62}
+                  height={62}
+                  className="rounded-full"
+                  src={post.postedBy.image}
+                  alt="profile photo"
+                  layout="responsive"
+                />
+              </>
+            </Link>
+          </div>
+          <div>
+            <Link href="/">
+              <div>
+                <p>{post.postedBy.userName}</p>{` `}
+                <GoVerified className="text-blue-400 text-md"/>
+              </div>
+            </Link>
+          </div>
+        </div>
       </div>
     </div>
   );
