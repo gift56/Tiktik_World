85d7def8fe226d9b71dc7e6f2c11f81a516d66d0
gift56 <efeasiughu@gmail.com>
2022-08-20 14:48:33 +0100

    successfully showhen gogle login button

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Image from "next/image";
 import Link from "next/link";
+import { GoogleLogin, googleLogout } from "@react-oauth/google";
 import { useRouter } from "next/router";
 import { AiOutlineLogout } from "react-icons/ai";
 import { BiSearch } from "react-icons/bi";
@@ -8,6 +9,7 @@ import { IoMdAdd } from "react-icons/io";
 import Logo from "../utils/tiktik-logo.png";
 
 const Navbar = () => {
+  const user = false;
   return (
     <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
       <Link href="/">
@@ -20,6 +22,17 @@ const Navbar = () => {
           />
         </div>
       </Link>
+      <div>Search</div>
+      <div>
+        {user ? (
+          <div>Logged in</div>
+        ) : (
+          <GoogleLogin
+            onSuccess={(res) => console.log(res)}
+            onError={() => console.log("error")}
+          />
+        )}
+      </div>
     </div>
   );
 };
