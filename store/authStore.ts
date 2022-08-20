ec766eeebb3dbb0fd4e52a2fbc5aaea007845447
gift56 <efeasiughu@gmail.com>
2022-08-20 15:40:08 +0100

    done with storage

--- store/authStore.ts
+++ store/authStore.ts
@@ -2,3 +2,16 @@ import create from "zustand";
 import { persist } from "zustand/middleware";
 import axios from "axios";
 
+const authStore = (set: any) => ({
+  userProfile: null,
+
+  addUser: (user: any) => set({ userProfile: user }),
+});
+
+const useAuthStore = create(
+  persist(authStore, {
+    name: "auth",
+  })
+);
+
+export default useAuthStore;
