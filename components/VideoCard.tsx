0dd9eca8938af902159911fc2ac5b38bf11dc957
gift56 <efeasiughu@gmail.com>
2022-08-20 12:15:57 +0100

    added new class name

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -58,11 +58,19 @@ const VideoCard: NextPage<IProps> = ({ post }) => {
             <video
               src={post.video.asset.url}
               loop
-              className="lg:w-[600px] h-[300px] md:h-[400px] lg:h-[500px] w-[200px] rounded-2xl cursor-pointer bg-gray-100"
+              className="lg:w-[600px] h-[300px] md:h-[400px] lg:h-[500px] w-[200px] rounded-2xl cursor-pointer bg-gray-100 object-cover"
             ></video>
           </Link>
           {isHover && <div>
-            
+            {playing?(
+                <button>
+                    <BsFillPauseFill/>
+                </button>
+            ):(
+                <button>
+                    <BsFillPlayFill/>
+                </button>
+            )}
             </div>}
         </div>
       </div>
