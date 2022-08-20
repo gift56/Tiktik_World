f34c47f5f014e4708764975aaeb0d42c1e6c2921
gift56 <efeasiughu@gmail.com>
2022-08-20 16:17:27 +0100

    done import some stiff

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 import { FaCloudDownloadAlt } from "react-icons/fa";
 import { MdDelete } from "react-icons/md";
+import axios from "axios";
+import useAuthStore from "../store/authStore";
+import { client } from "../utils/client";
 
 const Upload = () => {
   return <div>upload</div>;
