41ca494cc9f32bd0a656a43a55d6fe85545f3c77
gift56 <efeasiughu@gmail.com>
2022-08-20 16:30:59 +0100

    writing logics

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -7,6 +7,9 @@ import useAuthStore from "../store/authStore";
 import { client } from "../utils/client";
 
 const Upload = () => {
+  const [isLoading, setIsloading] = useState(false);
+
+  const [videoAsset, setVideoAsset] = useState();
   return (
     <div className="flex w-full h-full">
       <div className="bg-white rounded-lg">
@@ -15,7 +18,9 @@ const Upload = () => {
             <p className="text-2xl font-bold">Upload a Video</p>
             <p className="text-md text-gray-400">Post videos to your account</p>
           </div>
-          <div className="border-dashed rounded-xl border-4 border-gray-200 flex flex-col justify-center items-center outline-none mt-10 w-[260px] h-[460px] p-10 cursor-pointer hover:border-red-300 hover:bg-gray-100 transition-all transition-3s"></div>
+          <div className="border-dashed rounded-xl border-4 border-gray-200 flex flex-col justify-center items-center outline-none mt-10 w-[260px] h-[460px] p-10 cursor-pointer hover:border-red-300 hover:bg-gray-100 transition-all">
+            {isLoading ? <p>Uploading......</p> : <div></div>}
+          </div>
         </div>
       </div>
     </div>
