cd16a4e6815354ce6b0282d6f53e528b6ec10dbc
gift56 <efeasiughu@gmail.com>
2022-08-20 11:25:54 +0100

    done importing files

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,5 +1,10 @@
 import React, { useState, useEffect, useRef } from "react";
 import { NextPage } from "next";
+import Image from "next/image";
+import Link from "next/link";
+import { HiVolumeUp, HiVolumeOff } from "react-icons/hi";
+import {BsPlay, BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
+import { GoVerified } from "react-icons/go";
 import { Video } from "../types";
 
 interface IProps {
