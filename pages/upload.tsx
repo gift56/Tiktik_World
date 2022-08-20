2ddfeadd47b1695c9239defe8bd73015f2b3a86a
gift56 <efeasiughu@gmail.com>
2022-08-20 16:16:17 +0100

    import using icons

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -1,4 +1,7 @@
 import React, { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import { FaCloudDownloadAlt } from "react-icons/fa";
+import { MdDelete } from "react-icons/md";
 
 const Upload = () => {
   return <div>upload</div>;
