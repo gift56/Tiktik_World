7428520636c214770c539f7af0352d313ee702b6
gift56 <efeasiughu@gmail.com>
2022-08-20 16:24:19 +0100

    gap i border

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -12,9 +12,10 @@ const Upload = () => {
       <div className="bg-white rounded-lg">
         <div>
           <div>
-            <p>Upload a Video</p>
-            <p>Post videos to your account</p>
+            <p className="text-2xl font-bold">Upload a Video</p>
+            <p className="text-md text-gray-400">Post videos to your account</p>
           </div>
+          <div className="border-dashed rounded-xl border-4 border-gray-200 flex flex-col justify-center items-center outline-none mt-10 w-[260px]"></div>
         </div>
       </div>
     </div>
