ade37a1987b866342f3d37e2291dff1a3d837f8b
gift56 <efeasiughu@gmail.com>
2022-08-20 10:57:53 +0100

    successfully display video

--- pages/api/post/index.ts
+++ pages/api/post/index.ts
@@ -1,12 +1,16 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
 import { client } from "../../../utils/client";
 import { allPostsQuery } from "../../../utils/queries";
 
-export default function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
   if (req.method === "GET") {
     const query = allPostsQuery();
 
-    // const data = await client;
+    const data = await client.fetch(query);
+
+    res.status(200).json(data);
   }
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,23 @@
 import type { NextPage } from "next";
 import axios from "axios";
 
-const Home: NextPage = () => {
-  return <div className=""></div>;
+// interface IProps{
+//   videos:
+// }
+
+const Home: NextPage = ({videos}) => {
+  console.log(videos)
+
+  return <div className="">Hello Wolrd</div>;
 };
 
 export const getServerSideProps = async () => {
-  const response = await axios.get(`http://localhost:3000/api/post`);
-
-  console.log(response.data.name);
+  const { data } = await axios.get(`http://localhost:3000/api/post`);
 
   return {
-    props: {},
+    props: {
+      videos: data
+    },
   };
 };
 
