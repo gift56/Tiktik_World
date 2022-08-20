9e3a3ca9f73d3e81be28badace3a5e8d22997878
gift56 <efeasiughu@gmail.com>
2022-08-20 11:29:37 +0100

    started coding videocard

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -3,7 +3,7 @@ import { NextPage } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import { HiVolumeUp, HiVolumeOff } from "react-icons/hi";
-import {BsPlay, BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
+import { BsPlay, BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
 import { GoVerified } from "react-icons/go";
 import { Video } from "../types";
 
@@ -12,7 +12,13 @@ interface IProps {
 }
 
 const VideoCard: NextPage<IProps> = ({ post }) => {
-  return <div>VideoCard</div>;
+  return (
+    <div className="flex flex-col pb-6 border-b-2 border-gray-200">
+      <div>
+        
+      </div>
+    </div>
+  );
 };
 
 export default VideoCard;
