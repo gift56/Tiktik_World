7246b7f19bfd001233a24366e2b73bbdc69a5b5a
gift56 <efeasiughu@gmail.com>
2022-08-20 11:20:54 +0100

    Importing require stuff in videos

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -1,12 +1,12 @@
-import React from "react";
-import { Video } from "../types";
+import React, { useState, useEffect, useRef } from "react";
 import { NextPage } from "next";
+import { Video } from "../types";
 
 interface IProps {
   post: Video;
 }
 
-const VideoCard: NextPage<IProps> = ({post}) => {
+const VideoCard: NextPage<IProps> = ({ post }) => {
   return <div>VideoCard</div>;
 };
 
