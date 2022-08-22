66225ff6b7468b96cd195c58c6da54e873412460
gift56 <efeasiughu@gmail.com>
2022-08-22 15:33:35 +0100

    added container property

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -15,14 +15,18 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
   if (isSSR) return null;
 
   return (
-    <GoogleOAuthProvider clientId={`${process.env.NEXT_PUBLIC_GOGGLE_API_TOKEN}`}>
-      <Navbar />
-      <div className="flex g-6 md:gap-20">
-        <div className="h-[92vh] overflow-hidden xl:overflow-auto">
-          <Sidebar />
-        </div>
-        <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
-          <Component {...pageProps} />
+    <GoogleOAuthProvider
+      clientId={`${process.env.NEXT_PUBLIC_GOGGLE_API_TOKEN}`}
+    >
+      <div className="xl:w-[1200px] m-auto overflow-hidden h-[100vh]">
+        <Navbar />
+        <div className="flex g-6 md:gap-20">
+          <div className="h-[92vh] overflow-hidden xl:overflow-auto">
+            <Sidebar />
+          </div>
+          <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
+            <Component {...pageProps} />
+          </div>
         </div>
       </div>
     </GoogleOAuthProvider>
