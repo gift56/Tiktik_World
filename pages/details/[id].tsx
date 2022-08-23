5eede876b3c6fce33111586f886ad6adefac8066
gift56 <efeasiughu@gmail.com>
2022-08-23 09:30:55 +0100

    started orking today

--- pages/details/[id].tsx
+++ pages/details/[id].tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import React, { useState, useEffect, useRef } from "react";
+import { useRouter } from "next/router";
+import Image from "next/image";
+import Link from "next/link";
+import { GoVerified } from "react-icons/go";
+import { MdOutlineCancel } from "react-icons/md";
+import { BsFillPlayFill } from "react-icons/bs";
+import { HiVolumeUp, HiVolumeOff } from "react-icons/hi";
+import axios from "axios";
 
 const Detail = () => {
-  return (
-    <div>Detail</div>
-  )
-}
+  return <div>Detail</div>;
+};
 
-export default Detail
+const getServerSideProps = async ({ params: { id } }) => {
+  const { data } = axios.get("http://localhost:3000/api/post/${id}")
+};
+
+export default Detail;
