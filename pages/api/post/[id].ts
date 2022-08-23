78946623f9b6ca8d8bd4e6c9240341faadcce9f9
gift56 <efeasiughu@gmail.com>
2022-08-23 15:03:18 +0100

    getting back data

--- pages/api/post/[id].ts
+++ pages/api/post/[id].ts
@@ -1,10 +1,18 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { client } from "../../../utils/client";
+import { postDetailQuery } from "../../../utils/queries";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   if (req.method === "Get") {
-    
+    const { id } = req.query;
+
+    const query = postDetailQuery(id);
+
+    const data = await client.fetch(query);
+
+    res.status(200).json(data[0]);
   }
 }
