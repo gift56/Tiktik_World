409452954e12a4d946da82062156b2a2f4846460
gift56 <efeasiughu@gmail.com>
2022-08-23 17:51:39 +0100

    working on liked store

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { MdFavorite } from "react-icons/md";
+
+import useAuthStore from "../store/authStore";
 
 const LikeButton = () => {
-  return (
-    <div>
-      likebtn
-    </div>
-  )
-}
+  const [liked, setLiked] = useState(false);
+  return <div>likebtn</div>;
+};
 
-export default LikeButton
+export default LikeButton;
