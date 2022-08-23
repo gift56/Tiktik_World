93e78c0bcdd6de17ee3c8552a9dff6589daf364e
gift56 <efeasiughu@gmail.com>
2022-08-23 17:54:01 +0100

    adding styings

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -5,7 +5,15 @@ import useAuthStore from "../store/authStore";
 
 const LikeButton = () => {
   const [liked, setLiked] = useState(false);
-  return <div>likebtn</div>;
+  const { userProfile } = useAuthStore();
+
+  return (
+    <div className="gap-6">
+      <div className="flex flex-col justify-center items-center mt-4 cursor-pointer">
+        
+      </div>
+    </div>
+  );
 };
 
 export default LikeButton;
