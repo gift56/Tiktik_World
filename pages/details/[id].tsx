f4e0fa7860f7443a56ebe480262c23295696897a
gift56 <efeasiughu@gmail.com>
2022-08-23 16:39:47 +0100

    added bg img

--- pages/details/[id].tsx
+++ pages/details/[id].tsx
@@ -23,14 +23,16 @@ const Detail = ({ postDetails }: IProps) => {
 
   return (
     <div className="flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap">
-      <div className="relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-black-600">
+      <div className="relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-blurred-img bg-no-repeat bg-cover bg-center">
         <div className="absolute top-6 left-2 lg:left-6 flex gap-6 z-50">
           <p>
-            <MdOutlineCancel className="text-white text-[35px]"/>
+            <MdOutlineCancel className="text-white text-[35px]" />
           </p>
         </div>
-        <div>
-          
+        <div className="relative">
+          <div className="h-[60vh] lg:h-[100vh]">
+            <video src={post.video.asset.url}></video>
+          </div>
         </div>
       </div>
     </div>
