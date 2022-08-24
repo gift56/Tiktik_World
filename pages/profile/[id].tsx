5c4f4fc2e66b8f6b0fcdcb8b6fcabe72c9591298
gift56 <efeasiughu@gmail.com>
2022-08-24 15:37:45 +0100

    done with profile page

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -20,23 +20,23 @@ const Profile = ({ data }: IProps) => {
   return (
     <div>
       <div className="w-full">
-        <div className="flex gap-6 md:gap-10 mb-4 bg-white w-full">
-          <div className="w-16 h-16">
+        <div className="flex gap-6 md:gap-10 mb-4 bg-white w-full items-center">
+          <div className="w-16 h-16 md:w-32 md:h-32">
             <Image
               src={user.image}
-              width={34}
-              height={34}
+              width={120}
+              height={120}
               className="rounded-full"
               alt="user profile"
               layout="responsive"
             />
           </div>
-          <div className="hidden xl:block">
-            <p className="flex gap-1 items-center text-md font-bold text-primary lowercase">
+          <div className="flex flex-col justify-center">
+            <p className="flex gap-1 items-center text-md font-bold text-primary lowercase md:text-2xl tracking-wider justify-center">
               {user.userName.replace(" ", "")}{" "}
               <GoVerified className="text-blue-400" />
             </p>
-            <p className="flex gap-1 items-center text-xs font-bold text-gray-400 capitalize">
+            <p className="flex gap-1 items-center text-xs font-bold text-gray-400 capitalize md:text-xl">
               {user.userName}
             </p>
           </div>
