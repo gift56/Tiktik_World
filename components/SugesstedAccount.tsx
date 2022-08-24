4a69c690f0610962de7f19da7320de0afa0555a6
gift56 <efeasiughu@gmail.com>
2022-08-24 14:07:17 +0100

    importing required stuff in suggestedacc

--- components/SugesstedAccount.tsx
+++ components/SugesstedAccount.tsx
@@ -1,9 +1,8 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import Image from "next/image";
 
 const SugesstedAccount = () => {
-  return (
-    <div>SugesstedAccount</div>
-  )
-}
+  return <div>SugesstedAccount</div>;
+};
 
-export default SugesstedAccount
+export default SugesstedAccount;
