018ec1812266719fa28a11277453f48fa1ff441d
gift56 <efeasiughu@gmail.com>
2022-08-24 13:58:20 +0100

    working on suggested account

--- store/authStore.ts
+++ store/authStore.ts
@@ -9,7 +9,9 @@ const authStore = (set: any) => ({
   addUser: (user: any) => set({ userProfile: user }),
   removeUser: () => set({ userProfile: null }),
 
-  
+  fetchAllUsers: async () => {
+    const response = await axios.get(`${BASE_URL}/api/users`);
+  },
 });
 
 const useAuthStore = create(
