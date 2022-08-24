44b3d47cb430540857fc14f0fe6d76b6e8c074eb
gift56 <efeasiughu@gmail.com>
2022-08-24 13:54:53 +0100

    added some new stuff

--- store/authStore.ts
+++ store/authStore.ts
@@ -1,12 +1,15 @@
 import create from "zustand";
 import { persist } from "zustand/middleware";
 import axios from "axios";
+import { BASE_URL } from "../utils";
 
 const authStore = (set: any) => ({
   userProfile: null,
 
   addUser: (user: any) => set({ userProfile: user }),
   removeUser: () => set({ userProfile: null }),
+
+  
 });
 
 const useAuthStore = create(
