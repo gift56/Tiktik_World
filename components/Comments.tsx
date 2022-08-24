326293c9a7092dbdfd4fdff5cf117ef35e0a5456
gift56 <efeasiughu@gmail.com>
2022-08-24 11:53:03 +0100

    working on noresult components

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -6,8 +6,18 @@ import useAuthStore from "../store/authStore";
 import NoResult from "./NoResult";
 
 const Comments = () => {
+  const comments = [];
+
   return (
-    <div className="border-t-2 border-gray-200 pt-4 px-10 bg-[#f8f8f8] border-b-2 lg:pb-0 pb-[100px]"></div>
+    <div className="border-t-2 border-gray-200 pt-4 px-10 bg-[#f8f8f8] border-b-2 lg:pb-0 pb-[100px]">
+      <div className="overflow-scroll lg:h-[475px]">
+        {comments.length ? (
+          <div>Videos</div>
+        ) : (
+          <NoResult text="No Comments yet! Be the first to add a comment." />
+        )}
+      </div>
+    </div>
   );
 };
 
