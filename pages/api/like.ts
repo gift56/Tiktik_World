ab2b8f133d4ba21d34ea6c380adb030209f24058
gift56 <efeasiughu@gmail.com>
2022-08-24 11:08:41 +0100

    started working on like functionality

---
+++ pages/api/like.ts
@@ -0,0 +1,16 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+import { uuid } from "uuidv4";
+import { client } from "../../utils/client";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const user = req.body;
+
+    client
+      .createIfNotExists(user)
+      .then(() => res.status(200).json("Logged in Success"));
+  }
+}
