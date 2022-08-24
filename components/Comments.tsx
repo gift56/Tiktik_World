a2d54124724b01e874f079ed92aeb1159fb704f1
gift56 <efeasiughu@gmail.com>
2022-08-24 14:42:46 +0100

    just display who commentes

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -38,9 +38,31 @@ const Comments = ({
             <>
               {allUsers.map(
                 (user: IUser) =>
-                  user._id === (item.postedBy._id || item.postedBy._ref) &&(
+                  user._id === (item.postedBy._id || item.postedBy._ref) && (
                     <div className="p-2 items-center" key={index}>
-
+                      <Link href={`/profile/${user._id}`} key={user._id}>
+                        <div className="flex gap-3 items-start">
+                          <div className="w-8 h-8">
+                            <Image
+                              src={user.image}
+                              width={34}
+                              height={34}
+                              className="rounded-full"
+                              alt="user profile"
+                              layout="responsive"
+                            />
+                          </div>
+                          <div className="hidden xl:block">
+                            <p className="flex gap-1 items-center text-md font-bold text-primary lowercase">
+                              {user.userName.replace(" ", "")}{" "}
+                              <GoVerified className="text-blue-400" />
+                            </p>
+                            <p className="flex gap-1 items-center text-xs font-bold text-gray-400 capitalize">
+                              {user.userName}
+                            </p>
+                          </div>
+                        </div>
+                      </Link>
                     </div>
                   )
               )}
