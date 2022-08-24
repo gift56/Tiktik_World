deb29309a973d340e059cfed991cb86d7e9a7b73
gift56 <efeasiughu@gmail.com>
2022-08-24 14:59:07 +0100

    done import needed stuff

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -2,6 +2,10 @@ import React,{useState,useEffect} from "react";
 import Image from "next/image";
 import { GoVerified } from "react-icons/go";
 import axios from "axios";
+import VideoCard from "../../components/VideoCard";
+import NoResult from "../../components/NoResult";
+import { Video,IUser } from "../../types";
+import { BASE_URL } from "../../utils";
 
 const Profile = () => {
   return <div>Profile</div>;
