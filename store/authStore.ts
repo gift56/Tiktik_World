d4323a287ba02e203b408a1ad019923f92b41cbe
gift56 <efeasiughu@gmail.com>
2022-08-24 14:05:59 +0100

    using logic

--- store/authStore.ts
+++ store/authStore.ts
@@ -5,12 +5,15 @@ import { BASE_URL } from "../utils";
 
 const authStore = (set: any) => ({
   userProfile: null,
+  allUsers: [],
 
   addUser: (user: any) => set({ userProfile: user }),
   removeUser: () => set({ userProfile: null }),
 
   fetchAllUsers: async () => {
     const response = await axios.get(`${BASE_URL}/api/users`);
+
+    set({ allUsers: response.data });
   },
 });
 
