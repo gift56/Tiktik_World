ad3b66adcb39be9841c1029b80330f5ad4cd2ef8
gift56 <efeasiughu@gmail.com>
2022-08-24 14:18:16 +0100

    about mapping the first 5 users

--- components/SugesstedAccount.tsx
+++ components/SugesstedAccount.tsx
@@ -3,6 +3,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { GoVerified } from "react-icons/go";
 import useAuthStore from "../store/authStore";
+import { IUser } from "../types";
 
 const SugesstedAccount = () => {
   const { fetchAllUsers, allUsers } = useAuthStore();
@@ -13,7 +14,14 @@ const SugesstedAccount = () => {
 
   return (
     <div className="xl:border-b-2 border-gray-200 pb-4">
-      <p className="text-gray-500 font-semibold hidden m-3 mt-4 xl:block">Suggested Accounts</p>
+      <p className="text-gray-500 font-semibold hidden m-3 mt-4 xl:block">
+        Suggested Accounts
+      </p>
+      <div>
+        {allUsers.slice(0, 6).map((user: IUser) => (
+          <Link href={`/profile/${user._id}`} key={user._id}></Link>
+        ))}
+      </div>
     </div>
   );
 };
