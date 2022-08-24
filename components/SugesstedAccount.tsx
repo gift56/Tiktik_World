3fd7ecb067d5613d4e159ef892bfa21ead151e09
gift56 <efeasiughu@gmail.com>
2022-08-24 14:08:23 +0100

    done importing required stuff in suggestedacc

--- components/SugesstedAccount.tsx
+++ components/SugesstedAccount.tsx
@@ -1,5 +1,8 @@
 import React, { useEffect } from "react";
 import Image from "next/image";
+import Link from "next/link";
+import { GoVerified } from "react-icons/go";
+import useAuthStore from "../store/authStore";
 
 const SugesstedAccount = () => {
   return <div>SugesstedAccount</div>;
