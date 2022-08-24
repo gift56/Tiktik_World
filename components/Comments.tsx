cef27909c015f61f04ba5da5cda7389b7b7492ff
gift56 <efeasiughu@gmail.com>
2022-08-24 11:42:49 +0100

    started importing some few stuff

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import { GoVerified } from "react-icons/go";
+import useAuthStore from "../store/authStore";
+import NoResult from "./NoResult";
 
 const Comments = () => {
-  return (
-    <div>
-      Comments
-    </div>
-  )
-}
+  return <div>Comments</div>;
+};
 
-export default Comments
+export default Comments;
