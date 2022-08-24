f38f1995130d5bd03ef4fe0faaa6bfd05647d14d
gift56 <efeasiughu@gmail.com>
2022-08-24 15:05:24 +0100

    creating my end point

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -16,7 +16,7 @@ export const getServerSideProps = async ({
 }: {
   params: { id: string };
 }) => {
-  const res = await axios.get(`${BASE_URL}/profile/${id}`);
+  const res = await axios.get(`${BASE_URL}/api/profile/${id}`);
 };
 
 export default Profile;
