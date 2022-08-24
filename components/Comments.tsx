8b3651c802308f57fb35a8603dfd646814f816ba
gift56 <efeasiughu@gmail.com>
2022-08-24 11:45:20 +0100

    done importing some few stuff

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -6,7 +6,9 @@ import useAuthStore from "../store/authStore";
 import NoResult from "./NoResult";
 
 const Comments = () => {
-  return <div>Comments</div>;
+  return (
+    <div className="border-t-2 border-gray-200 pt-4 px-10 bg-[#f8f8f8] border-b-2 lg:pb-0 pb-[100px]"></div>
+  );
 };
 
 export default Comments;
