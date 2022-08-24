d522cbbc2515f616cc2db4551aa4458db24c9d39
gift56 <efeasiughu@gmail.com>
2022-08-24 15:06:26 +0100

    importing some stuff

--- pages/api/profile/[id].ts
+++ pages/api/profile/[id].ts
@@ -1,15 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { client } from "../../../utils/client";
+import { singleUserQuery } from "../../../utils/queries";
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method === "POST") {
-    const user = req.body;
-
-    client
-      .createIfNotExists(user)
-      .then(() => res.status(200).json("Logged in Success"));
+  if (req.method === "GET") {
+    
   }
 }
