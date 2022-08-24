ec960b2886addd595b9553b8316097c0ab7459e1
gift56 <efeasiughu@gmail.com>
2022-08-24 13:06:51 +0100

    import two new stuff from react

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Dispatch, SetStateAction } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { GoVerified } from "react-icons/go";
@@ -32,7 +32,7 @@ const Comments = ({
   return (
     <div className="border-t-2 border-gray-200 pt-4 px-10 bg-[#f8f8f8] border-b-2 lg:pb-0 pb-[100px]">
       <div className="overflow-scroll lg:h-[475px]">
-        {comments.length ? (
+        {comments?.length ? (
           <div>Video</div>
         ) : (
           <NoResult text="No Comments yet!" />
@@ -43,8 +43,8 @@ const Comments = ({
           <form onSubmit={() => {}} className="flex gap-4">
             <input
               type="text"
-              value=""
-              onChange={() => {}}
+              value={comment}
+              onChange={(e) => setComment(e.target.value.trim())}
               placeholder="Add comment..."
               className="bg-primary px-6 py-4 text-md font-medium border-2 w-[250px] md:w-[500px] lg:w-[350px] border-gray-100 focus:outline-none focus:border-2 focus:border-gray-300 flex-1 rounded-lg"
             />
