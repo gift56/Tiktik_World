e955ff0909bfe4c4dcbf77ce9a084a51c5b260b2
gift56 <efeasiughu@gmail.com>
2022-08-24 14:57:44 +0100

    importing rquired stuff profile area

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -1,4 +1,7 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
+import Image from "next/image";
+import { GoVerified } from "react-icons/go";
+import axios from "axios";
 
 const Profile = () => {
   return <div>Profile</div>;
