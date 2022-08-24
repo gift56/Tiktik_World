de7bfddaf073951292228854dd0e6bcf23ec62e7
gift56 <efeasiughu@gmail.com>
2022-08-24 15:02:34 +0100

    about fetching some stuff

--- pages/profile/[id].tsx
+++ pages/profile/[id].tsx
@@ -1,14 +1,22 @@
-import React,{useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Image from "next/image";
 import { GoVerified } from "react-icons/go";
 import axios from "axios";
 import VideoCard from "../../components/VideoCard";
 import NoResult from "../../components/NoResult";
-import { Video,IUser } from "../../types";
+import { Video, IUser } from "../../types";
 import { BASE_URL } from "../../utils";
 
 const Profile = () => {
   return <div>Profile</div>;
+};
+
+export const getServerSideProps = async ({
+  params: { id },
+}: {
+  params: { id: string };
+}) => {
+  const res = await axios.get(`${BASE_URL}/profile/${id}`);
 };
 
 export default Profile;
