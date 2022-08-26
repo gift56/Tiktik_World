5e9f4caa341b06a9e2515968bfe3f4872fe9257c
gift56 <efeasiughu@gmail.com>
2022-08-26 03:50:56 +0100

    made changes

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -3,7 +3,7 @@ import { NextPage } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import { HiVolumeUp, HiVolumeOff } from "react-icons/hi";
-import { BsPlay, BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
+import { BsFillPlayFill, BsFillPauseFill } from "react-icons/bs";
 import { GoVerified } from "react-icons/go";
 import { Video } from "../types";
 
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -32,7 +32,7 @@ const MyApp = ({ Component, pageProps }: AppProps) => {
               <div className="h-[92vh] overflow-hidden xl:overflow-auto">
                 <Sidebar />
               </div>
-              <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
+              <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1 ml-5">
                 <Component {...pageProps} />
               </div>
             </div>
