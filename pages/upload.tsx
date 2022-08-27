54d0e63ac663ffc70ec2a947df2333259a62f776
gift56 <efeasiughu@gmail.com>
2022-08-27 15:30:36 +0100

    adjusted some stuff

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -74,7 +74,7 @@ const Upload = () => {
         <div>
           <div>
             <p className="text-2xl font-bold">Upload a Video</p>
-            <p className="text-md text-gray-400">Post videos to your account</p>
+            <p className="text-md text-gray-400 dark:text-gray-300">Post videos to your account</p>
           </div>
           <div className="border-dashed rounded-xl border-4 border-gray-200 flex flex-col justify-center items-center outline-none mt-10 w-[260px] h-[460px] p-10 cursor-pointer hover:border-red-300 hover:bg-gray-100 dark:hover:bg-gray-600 transition-all">
             {isLoading ? (
