384b56aa32cda231652100cead8232f957b37ea1
gift56 <efeasiughu@gmail.com>
2022-11-07 17:09:19 +0100

    removing some stuff

--- pages/upload.tsx
+++ pages/upload.tsx
@@ -1,7 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/router";
 import { FaCloudDownloadAlt } from "react-icons/fa";
-import { MdDelete } from "react-icons/md";
 import axios from "axios";
 import useAuthStore from "../store/authStore";
 import { client } from "../utils/client";
